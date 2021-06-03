4edfbb80a8a7b33e733dc92b9362877ac7a783d8
Janice Koh <zapjanice@gmail.com>
2021-06-03 14:58:06 +0900

    Fix bugs and added new features to dataframe

--- textbook_project/clean_data.py
+++ textbook_project/clean_data.py
@@ -1,6 +1,3 @@
-!pip install git+git://github.com/linalgo/linalgo-sdk.git requests pillow
-%load_ext autoreload
-%autoreload 2
 import requests, io
 from PIL import Image
 import pandas as pd
@@ -9,15 +6,14 @@ import matplotlib.pyplot as plt
 
 from linalgo.annotate.models import Document, Annotation, Entity
 from linalgo.hub.client import LinalgoClient
-from linalgo.annotate.bbox import draw_bounding_boxes
 
 class Making_DF:
 
-    def __init__(self):    
+    def __init__(self, MYTOKEN):    
         client = LinalgoClient(token=MYTOKEN, api_url='https://prod.linhub.api.linalgo.com/v1')
         self.task = client.get_task('a9b4a03b-3af5-476f-8656-c69a32ea9866', verbose=True)
 
-    def making_list(task): 
+    def making_list(self, task): 
         ent_list =[]
         coord_list = []
         doc_list = []
@@ -32,7 +28,7 @@ class Making_DF:
                 doc_list.append(document.content)
             return  ent_list, coord_list, doc_list
 
-    def getting_coordinates(coord_list): 
+    def getting_coordinates(self, coord_list): 
         top= []
         bottom=[]
         left=[]
@@ -50,7 +46,7 @@ class Making_DF:
             height.append(coord_list[i].height)
         return top, bottom, left, right, area, width, height
 
-    def making_df(ent_list, coord_list, doc_list, top, bottom, left, right, area, width, height): 
+    def making_df(self, ent_list, coord_list, doc_list, top, bottom, left, right, area, width, height): 
         elem = {'Entity': ent_list, 'document_id': doc_list}
         df= pd.DataFrame(elem)
         df.columns = ['Entity', 'document_id']
@@ -69,16 +65,34 @@ class Making_DF:
         df['entity_count'] = df.groupby(['document_id', 'Entity']).cumcount()
         df = df.reset_index(drop=True)
         df = df.dropna()
+        df['aspect_ratio'] = pd.Series.abs(df['height'] / df['width'])
         return df
 
-    def clean_data(df): 
+    def clean_data(self, df): 
         keep_df = df[df['Entity'] == 'word-en']
         keep_s = keep_df.document_no.reset_index(drop=True).tolist()
         df = df[df['document_no'].isin(keep_s)]
         return df
     
+    def item_df(self, df):
+        item_df = df[df['Entity'] == 'item']
+        item_df.drop(labels=(['Entity','document_id']), axis = 1, inplace=True)
+        item_df.rename(columns = {'top':'top_item', 'bottom':'bottom_item', 'left':'left_item', 'right':'right_item', 
+                                  'width':'width_item', 'area':'area_item', 'height':'height_item','aspect_ratio': 'aspect_ratio_item'}, inplace = True)
+        return item_df
+    
+    def merge_df(self, df, item_df):
+        merge_df = df.merge(item_df, how='left', on=(['document_no', 'entity_count']))
+        merge_df['y_diff'] = pd.Series.abs(merge_df['top_item'] - merge_df['top'])
+        merge_df['x_diff'] = pd.Series.abs(merge_df['left_item'] - merge_df['left'])
+        featured_df = merge_df[['Entity', 'top', 'bottom', 'left', 'right', 'area', 'width', 'height', 'aspect_ratio', 'x_diff', 'y_diff']]
+        return featured_df    
+        
     def run_function(self):
-        ent_list, coord_list, doc_list = making_list(self.task)
-        top, bottom, left, right, area, width, height = getting_coordinates(coord_list)
-        df = making_df(ent_list, coord_list, doc_list, top, bottom, left, right, area, width, height)
-        return clean_data(df)
+        ent_list, coord_list, doc_list = self.making_list(self.task)
+        top, bottom, left, right, area, width, height = self.getting_coordinates(coord_list)
+        df = self.making_df(ent_list, coord_list, doc_list, top, bottom, left, right, area, width, height)
+        df = self.clean_data(df)
+        item_df = self.item_df(df)
+        featured_df = self.merge_df(df, item_df)
+        return featured_df 
