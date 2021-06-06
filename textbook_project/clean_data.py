e121234ecb76c00b8839845bf50060c3ab6a3537
Janice Koh <zapjanice@gmail.com>
2021-06-06 21:44:27 +0900

    Added code to connect clean data with getting label

--- textbook_project/clean_data.py
+++ textbook_project/clean_data.py
@@ -9,26 +9,27 @@ from linalgo.hub.client import LinalgoClient
 
 class Making_DF:
 
-    def __init__(self, MYTOKEN):    
+    def __init__(self, MYTOKEN):
         client = LinalgoClient(token=MYTOKEN, api_url='https://prod.linhub.api.linalgo.com/v1')
         self.task = client.get_task('a9b4a03b-3af5-476f-8656-c69a32ea9866', verbose=True)
 
-    def making_list(self, task): 
+    def making_list(self, docs):
         ent_list =[]
         coord_list = []
         doc_list = []
-        for i in range(len(task.documents)): 
-            document = task.documents[i]
-            for ant in range(len(document.annotations)): 
-                annotation = document.annotations[ant]
-                entity = annotation.entity.name
-                coordinates = annotation.target.selectors[0]
-                ent_list.append(entity)
-                coord_list.append(coordinates)
-                doc_list.append(document.content)
-            return  ent_list, coord_list, doc_list
 
-    def getting_coordinates(self, coord_list): 
+        for document in docs:
+            annotator_name = document.annotations[0].annotator.name
+            for annotation in document.annotations:
+                if annotation.annotator.name == annotator_name:
+                    doc_list.append(document)
+                    ent_list.append(annotation.entity.name)
+                    coord_list.append(annotation.target.selectors)
+
+        return ent_list, coord_list, doc_list
+
+
+    def getting_coordinates(self, coord_list):
         top= []
         bottom=[]
         left=[]
@@ -36,20 +37,26 @@ class Making_DF:
         area = []
         width=[]
         height=[]
-        for i in range(len(coord_list)):
-            top.append(coord_list[i].top)
-            bottom.append(coord_list[i].bottom)
-            left.append(coord_list[i].left)
-            right.append(coord_list[i].right)
-            area.append(coord_list[i].area)
-            width.append(coord_list[i].width)
-            height.append(coord_list[i].height)
+
+        for bb in range(len(coord_list)):
+            for i in range(len(coord_list[bb])):
+                top.append(coord_list[bb][i].top)
+                bottom.append(coord_list[bb][i].bottom)
+                left.append(coord_list[bb][i].left)
+                right.append(coord_list[bb][i].right)
+                area.append(coord_list[bb][i].area)
+                width.append(coord_list[bb][i].width)
+                height.append(coord_list[bb][i].height)
+
         return top, bottom, left, right, area, width, height
 
-    def making_df(self, ent_list, coord_list, doc_list, top, bottom, left, right, area, width, height): 
+    def making_df(self, ent_list, doc_list, top, bottom, left, right, area, width, height):
+        pd.options.display.max_colwidth = 200
         elem = {'Entity': ent_list, 'document_id': doc_list}
         df= pd.DataFrame(elem)
         df.columns = ['Entity', 'document_id']
+        df['Entity'] = df.Entity.astype(str)
+        df['document_id'] = df.document_id.astype(str)
 
         df['document_no'] = df.groupby(['document_id']).ngroup()
 
@@ -64,38 +71,48 @@ class Making_DF:
         df = df.sort_values(by=['document_no', 'top'])
         df['entity_count'] = df.groupby(['document_id', 'Entity']).cumcount()
         df = df.reset_index(drop=True)
-        df = df.dropna()
         df['aspect_ratio'] = pd.Series.abs(df['height'] / df['width'])
         return df
 
-    def clean_data(self, df): 
+    def clean_data(self, df):
         keep_df = df[df['Entity'] == 'word-en']
         keep_s = keep_df.document_no.reset_index(drop=True).tolist()
         df = df[df['document_no'].isin(keep_s)]
+        df = df[df['Entity'] != 'None']
         return df
-    
+
     def item_df(self, df):
         item_df = df[df['Entity'] == 'item']
         item_df.drop(labels=(['Entity','document_id']), axis = 1, inplace=True)
-        item_df.rename(columns = {'top':'top_item', 'bottom':'bottom_item', 'left':'left_item', 'right':'right_item', 
-                                  'width':'width_item', 'area':'area_item', 'height':'height_item',
-                                  'aspect_ratio': 'aspect_ratio_item'}, inplace = True)
+        item_df.rename(columns = {'top':'top_item', 'bottom':'bottom_item', 'left':'left_item',
+                                  'right':'right_item', 'width':'width_item', 'area':'area_item',
+                                  'height':'height_item', 'aspect_ratio': 'aspect_ratio_item'}, inplace = True)
         return item_df
-    
+
     def merge_df(self, df, item_df):
         merge_df = df.merge(item_df, how='left', on=(['document_no', 'entity_count']))
         merge_df['y_diff'] = pd.Series.abs(merge_df['top_item'] - merge_df['top'])
         merge_df['x_diff'] = pd.Series.abs(merge_df['left_item'] - merge_df['left'])
         merge_df['x_diff'].fillna(0, inplace = True)
         merge_df['y_diff'].fillna(0, inplace = True)
-        featured_df = merge_df[['Entity', 'top', 'bottom', 'left', 'right', 'area', 'width', 'height', 'aspect_ratio', 'x_diff', 'y_diff']]
-        return featured_df    
-        
+        featured_df = merge_df[[
+            'Entity', 'document_id', 'top', 'bottom', 'left',
+            'right', 'area', 'width', 'height',
+            'aspect_ratio', 'x_diff', 'y_diff'
+        ]]
+        return featured_df
+
     def run_function(self):
-        ent_list, coord_list, doc_list = self.making_list(self.task)
-        top, bottom, left, right, area, width, height = self.getting_coordinates(coord_list)
-        df = self.making_df(ent_list, coord_list, doc_list, top, bottom, left, right, area, width, height)
-        df = self.clean_data(df)
-        item_df = self.item_df(df)
-        featured_df = self.merge_df(df, item_df)
-        return featured_df 
+        task = self.task
+        total_set = task.documents
+        total_set_list = [total_set]
+        featured_df_list = []
+        for docs in total_set_list:
+            ent_list, coord_list, doc_list = self.making_list(docs=docs)
+            top, bottom, left, right, area, width, height = self.getting_coordinates(coord_list)
+            df = self.making_df(ent_list, doc_list, top, bottom, left, right, area, width, height)
+            df = self.clean_data(df)
+            item_df = self.item_df(df)
+            featured_df = self.merge_df(df, item_df)
+            featured_df_list.append(featured_df)
+        return featured_df_list[0]
